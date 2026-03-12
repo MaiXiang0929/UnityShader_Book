32398b03dfb67047ca1dbb85cf499308156eb254
Zhenhui Tao <2679664405@qq.com>
2026-03-12 14:04:31 +0800

    improve

--- UnityShader/Assets/Scripts/ShaderBase/Chapter10/ProceduralTextureGeneration.cs
+++ UnityShader/Assets/Scripts/ShaderBase/Chapter10/ProceduralTextureGeneration.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
-using SetPropertyExample;
 
-[ExecuteInEditModel]
+[ExecuteInEditMode]
 public class ProceduralTextureGeneration : MonoBehaviour
 {
     public Material material = null;
@@ -11,7 +11,25 @@ public class ProceduralTextureGeneration : MonoBehaviour
     #region Material properties
     [SerializeField, SetProperty("textureWidth")]
     private int m_textureWidth = 512;
-    public int textureWidth {}
+    public int textureWidth
+    {
+        get { return m_textureWidth; }
+        set
+        {
+            m_textureWidth = value;
+            _UpdateMaterial();
+        }
+    }
 
     #endregion
+
+    private void _UpdateMaterial()
+{
+    if (material != null)
+    {
+       
+    }
 }
+}
+
+
